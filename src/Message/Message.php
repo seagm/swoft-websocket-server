a5d78264935d5d973eb8205da1367596d666a5f4
QD WEN <44762586+wqd1688@users.noreply.github.com>
2025-11-23 10:53:52 +0800

    compatibility

--- src/Message/Message.php
+++ src/Message/Message.php
@@ -153,7 +153,7 @@ class Message implements JsonSerializable
      * which is a value of any type other than a resource.
      * @since 5.4.0
      */
-    public function jsonSerialize()
+    public function jsonSerialize(): mixed
     {
         return $this->toArray();
     }
